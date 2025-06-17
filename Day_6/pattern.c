a2752de53e3929d364cce9a4307c90db8d3597b9
Sanjay-083 <sanjaymayakrishnan083@gmail.com>
2025-06-17 10:45:09 +0530

    pt

--- Day_6/pattern.c
+++ Day_6/pattern.c
@@ -9,7 +9,7 @@ by using a n and i to contruct S and J
 for right angle:
 s=(n-(i+1))
 j=(i+1)
-*/
+
 #include<stdio.h>
 void main()
 {
@@ -26,7 +26,36 @@ void main()
             printf("*");
         }
     printf("\n");
-        
+
     }
-    
-}
+}*/
+/*
+*  *  *  *  *  *
+*              *
+*              *
+*              *
+*              *
+*  *  *  *  *  *
+#include <stdio.h>
+void main()
+{
+    int n = 5;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (j == 0 || i == 0 || i == n || j == n)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+
+        }
+        printf("\n");
+        }
+
+}*/
+
