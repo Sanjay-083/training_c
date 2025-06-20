6cbe5b53a765fe780e88207e7a45b77538e5a1f2
Sanjay-083 <sanjaymayakrishnan083@gmail.com>
2025-06-20 19:52:15 +0530

    .

---
+++ day_10/prime.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int prime(int n)
+{
+    int co = 0;
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            co++;
+        }
+    }
+}
+void main()
+{
+    int nu;
+    
+    
+}
