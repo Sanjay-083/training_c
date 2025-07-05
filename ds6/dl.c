d7947f3ba9266f89b6185cdb8e57a3383a73a5ed
Sanjay-083 <sanjaymayakrishnan083@gmail.com>
2025-07-05 10:13:39 +0530

    .

--- ds6/dl.c
+++ ds6/dl.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct Node
 {
     int data;
     struct Node *next;
+    struct Node *prev;
 };
+
 void main()
 {
     struct Node *head = NULL;
     struct Node *createNode = NULL;
     struct Node *new = NULL;
-    // // need to TypeCAsting Else it will get return a VOID POINTER so type casting is important in malloc
-    //  createNode = (struct Node*)malloc(sizeof(struct Node));
-    // // Typecasting is used here to convert the void pointer returned by malloc to a pointer of type struct Node.
-    // // This ensures that createNode is of the correct type and can be used to access struct members.
-    // createNode->data = 10;
-    // createNode->next = NULL;
-    // head = createNode;
-    // printf("%p",head->next);
-    // /*
-    //  The Above Code is used to create a Node and Head then insert a Data
-    // */w
+    struct Node *prev = NULL;
     int node;
+
     printf("Enter the Number of Nodes: ");
     scanf("%d", &node);
+
     for (int i = 0; i < node; i++)
     {
         createNode = (struct Node *)malloc(sizeof(struct Node));
@@ -32,6 +27,8 @@ void main()
         scanf("%d", &value);
         createNode->data = value;
         createNode->next = NULL;
+        createNode->prev = NULL;
+
         if (head == NULL)
         {
             head = createNode;
@@ -39,13 +36,14 @@ void main()
         else
         {
             new->next = createNode;
+            createNode->prev = new;
         }
         new = createNode;
     }
     struct Node *temp = head;
     while (temp != NULL)
     {
-        printf("%d ", temp->data);
+        printf("%d", temp->data);
         temp = temp->next;
     }
 }
